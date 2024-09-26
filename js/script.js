d54f86e2e14a5688954cb1f889c812be65b1120d
Leleco04 <htwleandro@gmail.com>
2024-09-26 15:59:56 -0300

    Ultimo

--- js/script.js
+++ js/script.js
@@ -6,7 +6,7 @@ document.addEventListener("DOMContentLoaded", function () {
   botao.addEventListener("click", function () {
     let pokemon = display.value;
     const url = `https://pokeapi.co/api/v2/pokemon/${pokemon}`;
-    const nome = document.getElementById("nome") 
+    const nome = document.getElementById("nome")
     const peso = document.getElementById("peso")
     const altura = document.getElementById("altura")
     const visor = document.getElementById("visor")
@@ -24,10 +24,11 @@ document.addEventListener("DOMContentLoaded", function () {
     const barra5 = document.getElementById("defesa-especial-barra")
     const barra6 = document.getElementById("velocidade-barra")
     const barras = document.getElementById("barras")
+    const arrayBarras = [barra1, barra2, barra3, barra4, barra5, barra6]
 
     fetch(url)
       .then(response => {
-        if(!response.ok) {
+        if (!response.ok) {
           throw new Error("Erro ao realizar API: " + response.status)
         }
         return response.json()
@@ -40,19 +41,19 @@ document.addEventListener("DOMContentLoaded", function () {
         barras.classList.add("barras-mostrar")
         const divImg = document.createElement("div")
         divImg.classList.add("div-imagem")
-        divImg.innerHTML = `<img src="${data.sprites.other.showdown.front_default}" class="imagem-pokemon">`
+        divImg.innerHTML = `<img src="${data.sprites.other.dream_world.front_default}" class="imagem-pokemon">`
         visor.appendChild(divImg)
 
         let alturaPokemon = (data.height / 10)
         let pesoPokemon = (data.weight / 10)
         // let hp = data.stats[0]['base_stat']
         let status = []
-        for(let x = 0; x < 6; x++) {
+        for (let x = 0; x < 6; x++) {
           status[x] = data.stats[x]['base_stat']
         }
 
         nome.innerText = `${data.name}`
-        peso.innerText = `${pesoPokemon} kg` 
+        peso.innerText = `${pesoPokemon} kg`
         altura.innerHTML = `${alturaPokemon} metros`
         hpNum.innerText = status[0]
         ataqueNum.innerText = status[1]
@@ -61,12 +62,21 @@ document.addEventListener("DOMContentLoaded", function () {
         defesaEspecialNum.innerText = status[4]
         velocidadeNum.innerText = status[5]
 
-        barra1.style.paddingRight = status[0] + "px"
-        barra2.style.paddingRight = status[1] + "px"
-        barra3.style.paddingRight = status[2] + "px"
-        barra4.style.paddingRight = status[3] + "px"
-        barra5.style.paddingRight = status[4] + "px"
-        barra6.style.paddingRight = status[5] + "px"
+        document.body.style.background = `url(${data.sprites.other.dream_world.front_default})`
+
+        for (let i = 0; i < status.length; i++) {
+          arrayBarras[i].style.paddingRight = status[i] + "px"
+        }
+
+        for (let iStatus = 0; iStatus < status.length; iStatus++) {
+          if (status[iStatus] <= 35) {
+            arrayBarras[iStatus].style.backgroundColor = "red"
+          } else if (status[iStatus] > 35 && status[iStatus] <= 70) {
+            arrayBarras[iStatus].style.backgroundColor = "#ffed00"
+          } else {
+            arrayBarras[iStatus].style.backgroundColor = "lightgreen"
+          }
+        }
 
       })
       .catch(error => {
